a345ffb74ee4437bed6ea8974c421b9552dc6773
NOL10 <noelgeorge1012@gmail.com>
2026-03-10 14:37:29 +0530

    Changes and SEO error handling async

--- src/components/camp/seo.jsx
+++ src/components/camp/seo.jsx
@@ -8,7 +8,7 @@ export function CampSEO() {
       <meta name="description" content="Join Flowternity's Summer Sports Camps 2026! Professional basketball, skating, and futsal training for kids and teens. Multi-sport facility in Horamavu, Bengaluru. Register now for early bird discounts!" />
 
       {/* Keywords */}
-      <meta name="keywords" content="summer sports camps, basketball camp, skating camp, futsal camp, kids sports, Bengaluru sports, Horamavu, multi-sport facility, Flowternity, summer camp 2026, basketball training, skating classes, futsal coaching" />
+      <meta name="keywords" content="summer sports camps, basketball camp, skating Camp, futsal Camp, kids sports, Bengaluru sports, Horamavu, multi-sport facility, Flowternity, summer camp 2026, basketball training, skating classes, futsal coaching" />
 
       {/* Open Graph */}
       <meta property="og:title" content="Summer Sports Camps 2026 | Flowternity Sports" />
@@ -34,47 +34,6 @@ export function CampSEO() {
       <meta name="geo.region" content="Karnataka" />
       <meta name="geo.placename" content="Bengaluru" />
       <meta name="geo.position" content="13.0352392;77.6679704" />
-
-      {/* JSON-LD Structured Data */}
-      <script type="application/ld+json">
-        {`{
-          "@context": "https://schema.org",
-          "@type": "SportsEvent",
-          "name": "Flowternity Summer Sports Camps 2026",
-          "description": "Professional summer sports camps offering basketball, skating, and futsal training for kids and teens in Bengaluru.",
-          "url": "https://flowternity.com/summer-camp",
-          "startDate": "2026-04-06",
-          "endDate": "2026-05-14",
-          "location": {
-            "@type": "Place",
-            "name": "Flowternity Sports",
-            "address": "1456, Old Flour Mill Road, Dodda Kempaih Layout, Kalkere, Horamavu, Bengaluru, Karnataka 560043",
-            "geo": {
-              "@type": "GeoCoordinates",
-              "latitude": 13.0352392,
-              "longitude": 77.6679704
-            }
-          },
-          "offers": {
-            "@type": "Offer",
-            "price": "3999",
-            "priceCurrency": "INR",
-            "availability": "https://schema.org/InStock",
-            "validFrom": "2026-03-01"
-          },
-          "organizer": {
-            "@type": "Organization",
-            "name": "Flowternity Sports",
-            "url": "https://flowternity.com",
-            "logo": "https://flowternity.com/images/logo.png"
-          },
-          "eventAttendanceMode": "https://schema.org/OfflineEventAttendanceMode",
-          "performer": {
-            "@type": "Organization",
-            "name": "Flowternity Sports Coaching Staff"
-          }
-        }`}
-      </script>
     </Helmet>
   )
 }
