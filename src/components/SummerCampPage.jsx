3682a51756a7603fc6d14fd3abca47f90731dd35
NOL10 <noelgeorge1012@gmail.com>
2026-03-10 15:21:21 +0530

    better seo

--- src/components/SummerCampPage.jsx
+++ src/components/SummerCampPage.jsx
@@ -18,8 +18,8 @@ const SummerCampPage = () => {
   const structuredData = {
     "@context": "https://schema.org",
     "@type": "SportsEvent",
-    "name": "Flowternity Summer Sports Camp",
-    "description": "Multi-sport summer camp featuring basketball, futsal, skating, skateboarding, calisthenics, and karate for kids and teens",
+    "name": "Flowternity Summer Sports Camp 2026",
+    "description": "Multi-sport summer camp 2026 featuring basketball, futsal, skating, skateboarding, calisthenics, and karate for kids and teens",
     "url": "https://join.sportomic.com/flowternity-sports/summer-camp",
     "location": {
       "@type": "Place",
@@ -30,8 +30,8 @@ const SummerCampPage = () => {
         "addressCountry": "IN"
       }
     },
-    "startDate": "2024-05-01",
-    "endDate": "2024-07-31",
+    "startDate": "2026-05-01",
+    "endDate": "2026-07-31",
     "offers": {
       "@type": "Offer",
       "price": "4999",
@@ -49,25 +49,25 @@ const SummerCampPage = () => {
     <>
       <Helmet>
         {/* Primary Meta Tags */}
-        <title>Summer Sports Camp 2024 | Flowternity Multi-Sport Facility Bangalore</title>
-        <meta name="description" content="Join Flowternity's exciting summer sports camp! Basketball, futsal, skating, skateboarding, calisthenics & karate. International standard facilities. Ages 6-16. Register now!" />
-        <meta name="keywords" content="summer camp Bangalore, sports camp for kids, basketball camp, futsal training, skating classes, skateboarding lessons, calisthenics for children, karate classes, multi-sport camp, Bangalore sports activities" />
+        <title>Summer Camp 2026 | Flowternity Multi-Sport Facility Bangalore</title>
+        <meta name="description" content="Join Flowternity's exciting summer camp 2026! Basketball, futsal, skating, skateboarding, calisthenics & karate. International standard facilities. Ages 6-16. Register now!" />
+        <meta name="keywords" content="summer camp 2026, sports camp 2026 Bangalore, summer camp Bangalore 2026, kids summer camp 2026, basketball camp 2026, futsal training 2026, skating classes 2026, skateboarding lessons 2026, calisthenics for children 2026, karate classes 2026, multi-sport camp 2026, Bangalore summer activities 2026, summer sports camp 2026" />
         <meta name="author" content="Flowternity Sports" />
         <link rel="canonical" href="https://join.sportomic.com/flowternity-sports/summer-camp" />
 
         {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="Summer Sports Camp 2024 | Flowternity Multi-Sport Facility" />
-        <meta property="og:description" content="Join Flowternity's exciting summer sports camp! Basketball, futsal, skating, skateboarding, calisthenics & karate. International standard facilities. Ages 6-16." />
+        <meta property="og:title" content="Summer Camp 2026 | Flowternity Multi-Sport Facility" />
+        <meta property="og:description" content="Join Flowternity's exciting summer camp 2026! Basketball, futsal, skating, skateboarding, calisthenics & karate. International standard facilities. Ages 6-16." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://join.sportomic.com/flowternity-sports/summer-camp" />
         <meta property="og:image" content="https://playverseproduction.blob.core.windows.net/venue-logos/venue-logos/1762528502281-Logo_Basketball.jpg.jpg" />
-        <meta property="og:image:alt" content="Flowternity Summer Sports Camp - Multi-sport facility for kids" />
+        <meta property="og:image:alt" content="Flowternity Summer Camp 2026 - Multi-sport facility for kids" />
         <meta property="og:site_name" content="Flowternity Sports" />
 
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Summer Sports Camp 2024 | Flowternity Multi-Sport Facility" />
-        <meta name="twitter:description" content="Join Flowternity's exciting summer sports camp! Basketball, futsal, skating, skateboarding & more. Register now!" />
+        <meta name="twitter:title" content="Summer Camp 2026 | Flowternity Multi-Sport Facility" />
+        <meta name="twitter:description" content="Join Flowternity's exciting summer camp 2026! Basketball, futsal, skating, skateboarding & more. Register now!" />
         <meta name="twitter:image" content="https://playverseproduction.blob.core.windows.net/venue-logos/venue-logos/1762528502281-Logo_Basketball.jpg.jpg" />
 
         {/* Additional SEO Meta Tags */}
@@ -76,6 +76,11 @@ const SummerCampPage = () => {
         <meta name="geo.region" content="IN-KA" />
         <meta name="geo.placename" content="Bangalore" />
         <meta name="ICBM" content="12.9716,77.5946" />
+        <meta name="date" content="2026-05-01" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="distribution" content="global" />
+        <meta name="rating" content="general" />
+        <meta name="expires" content="never" />
 
         {/* Structured Data */}
         <script type="application/ld+json">
